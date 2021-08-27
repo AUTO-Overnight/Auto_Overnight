418654e41974b9a9c4a7e31fd991d74c0732e599
ww8007 <wshmin@naver.com>
2021-08-27 16:46:10 +0900

    Feat : add export * in components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from './TouchableView';
+export * from './Avatar';
+export * from './IconText';
+export * from './ImageSlider';
+export * from './LeftSwipe';
+export * from './LeftRightNavigation';
