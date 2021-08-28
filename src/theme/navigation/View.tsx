e5b45b5a4570ae4d8d121e6bd7cadb0374c50fc0
ww8007 <wshmin@naver.com>
2021-08-28 20:45:36 +0900

    Design : add darkTheme style

--- src/theme/navigation/View.tsx
+++ src/theme/navigation/View.tsx
@@ -1,37 +1,41 @@
 import React from 'react';
-import type {FC, ComponentProps} from 'react';
-import {View as RNView} from 'react-native';
-import {useTheme} from '@react-navigation/native';
+import type { FC, ComponentProps } from 'react';
+import { View as RNView } from 'react-native';
+import { useTheme } from '@react-navigation/native';
 
 export type ViewProps = ComponentProps<typeof RNView> & {
-  border?: boolean;
-  card?: boolean;
-  primary?: boolean;
-  notification?: boolean;
+	border?: boolean;
+	card?: boolean;
+	primary?: boolean;
+	notification?: boolean;
+	blue?: string;
 };
 
 export const View: FC<ViewProps> = ({
-  border,
-  card,
-  primary,
-  notification,
-  style,
-  ...props
+	border,
+	card,
+	primary,
+	notification,
+	style,
+	blue,
+	...props
 }) => {
-  const {colors} = useTheme();
-  const backgroundColor = card
-    ? colors.card
-    : primary
-    ? colors.primary
-    : notification
-    ? colors.notification
-    : colors.background;
-  const borderColor = border ? colors.border : undefined;
-  const borderWidth = border ? 1 : undefined;
-  return (
-    <RNView
-      style={[{backgroundColor, borderColor, borderWidth}, style]}
-      {...props}
-    />
-  );
+	const { colors } = useTheme();
+	const backgroundColor = card
+		? colors.card
+		: colors.border
+		? primary
+			? colors.primary
+			: notification
+			? colors.border
+			: colors.background
+		: colors.border;
+	const borderColor = border ? colors.border : undefined;
+	const borderWidth = border ? 1 : undefined;
+	return (
+		<RNView
+			style={[{ backgroundColor, borderColor, borderWidth }, style]}
+			{...props}
+		/>
+	);
 };
