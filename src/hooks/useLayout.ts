db0ac0d87731dd226f5415c07c150be80eb433ff
ww8007 <wshmin@naver.com>
2021-08-29 22:59:03 +0900

    Feat : add useLayout custom Hook getRectangle

---
+++ src/hooks/useLayout.ts
@@ -0,0 +1,20 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import type { LayoutChangeEvent, LayoutRectangle } from 'react-native';
+import { Animated } from 'react-native';
+
+export const useLayout = (): [
+	LayoutRectangle,
+	(e: LayoutChangeEvent) => void
+] => {
+	const [layout, setLayout] = useState<LayoutRectangle>({
+		x: 0,
+		y: 0,
+		width: 0,
+		height: 0,
+	});
+	const onLayout = useCallback((e: LayoutChangeEvent) => {
+		const { layout } = e.nativeEvent;
+		setLayout(layout);
+	}, []);
+	return [layout, onLayout];
+};
