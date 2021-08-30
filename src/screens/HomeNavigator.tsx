a9bc323eab1ea584254635ac372d8c18e8e6a579
ww8007 <wshmin@naver.com>
2021-08-31 02:19:06 +0900

    Fix : Stack navi route component Home Week

--- src/screens/HomeNavigator.tsx
+++ src/screens/HomeNavigator.tsx
@@ -1,42 +1,35 @@
-import React, {useMemo} from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import type {StackNavigationOptions} from '@react-navigation/stack';
-import {useNavigationHorizontalInterpolator} from '../hooks';
+import React, { useMemo } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import type { StackNavigationOptions } from '@react-navigation/stack';
+import { useNavigationHorizontalInterpolator } from '../hooks';
 import Home from './Home';
 import HomeLeft from './HomeLeft';
 import HomeRight from './HomeRight';
+import Week from './Week';
 
 const Stack = createStackNavigator();
 
 export default function MainNavigator() {
-  const interpolator = useNavigationHorizontalInterpolator();
-  const leftOptions = useMemo<StackNavigationOptions>(
-    () => ({
-      gestureDirection: 'horizontal-inverted',
-      cardStyleInterpolator: interpolator,
-    }),
-    [],
-  );
-  const rightOptions = useMemo<StackNavigationOptions>(
-    () => ({
-      gestureDirection: 'horizontal',
-      cardStyleInterpolator: interpolator,
-    }),
-    [],
-  );
-  return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen
-        name="HomeLeft"
-        component={HomeLeft}
-        options={leftOptions}
-      />
-      <Stack.Screen
-        name="HomeRight"
-        component={HomeRight}
-        options={rightOptions}
-      />
-    </Stack.Navigator>
-  );
+	const interpolator = useNavigationHorizontalInterpolator();
+	const leftOptions = useMemo<StackNavigationOptions>(
+		() => ({
+			gestureDirection: 'horizontal-inverted',
+			cardStyleInterpolator: interpolator,
+		}),
+		[]
+	);
+	const rightOptions = useMemo<StackNavigationOptions>(
+		() => ({
+			gestureDirection: 'horizontal',
+			cardStyleInterpolator: interpolator,
+		}),
+		[]
+	);
+	return (
+		<Stack.Navigator screenOptions={{ headerShown: false }}>
+			{/* <Stack.Screen name="Home" component={Home} /> */}
+			<Stack.Screen name="Home" component={Home} options={leftOptions} />
+			<Stack.Screen name="Week" component={Week} options={rightOptions} />
+		</Stack.Navigator>
+	);
 }
