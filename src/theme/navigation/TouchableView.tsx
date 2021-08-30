4cfa276fc53c424eebd5b423bbb62254337388bf
ww8007 <wshmin@naver.com>
2021-08-31 02:20:37 +0900

    Fix : View import dir

--- src/theme/navigation/TouchableView.tsx
+++ src/theme/navigation/TouchableView.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
-import type {FC} from 'react';
-import {StyleSheet, TouchableOpacity} from 'react-native';
-import type {StyleProp, ViewStyle} from 'react-native';
-import {View} from './View';
-import type {ViewProps} from './View';
+import type { FC } from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import type { StyleProp, ViewStyle } from 'react-native';
+import { View } from '../paper/View';
+import type { ViewProps } from '../paper/View';
 
 export type TouchableViewProps = ViewProps & {
-  onPress?: () => void;
-  touchableStyle?: StyleProp<ViewStyle>;
+	onPress?: () => void;
+	touchableStyle?: StyleProp<ViewStyle>;
 };
 
 export const TouchableView: FC<TouchableViewProps> = ({
-  children,
-  onPress,
-  touchableStyle,
-  ...viewProps
+	children,
+	onPress,
+	touchableStyle,
+	...viewProps
 }) => {
-  return (
-    <TouchableOpacity
-      onPress={onPress}
-      style={[styles.touchable, touchableStyle]}>
-      <View {...viewProps}>{children}</View>
-    </TouchableOpacity>
-  );
+	return (
+		<TouchableOpacity
+			onPress={onPress}
+			style={[styles.touchable, touchableStyle]}
+		>
+			<View {...viewProps}>{children}</View>
+		</TouchableOpacity>
+	);
 };
 const styles = StyleSheet.create({
-  touchable: {width: '100%', alignItems: 'center', justifyContent: 'center'},
+	touchable: { width: '100%', alignItems: 'center', justifyContent: 'center' },
 });
