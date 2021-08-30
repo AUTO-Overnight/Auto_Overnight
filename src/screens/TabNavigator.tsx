21630607fea9061ffd0c5a180a453c94aae21637
ww8007 <wshmin@naver.com>
2021-08-31 02:19:38 +0900

    Feat : add Week page, Icon

--- src/screens/TabNavigator.tsx
+++ src/screens/TabNavigator.tsx
@@ -1,39 +1,60 @@
-import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Colors} from 'react-native-paper';
+import React, { useMemo } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Colors } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeNavigator from './HomeNavigator';
-import type {RouteProp, ParamListBase} from '@react-navigation/native';
+import Week from './Week';
+import type { RouteProp, ParamListBase } from '@react-navigation/native';
+import Home from './Home';
+import { useNavigationHorizontalInterpolator } from '../hooks';
+import { StackNavigationOptions } from '@react-navigation/stack';
 
-type TabBarIconProps = {focused: boolean; color: string; size: number};
+type TabBarIconProps = { focused: boolean; color: string; size: number };
 
 const icons: Record<string, string[]> = {
-  HomeNavigator: ['home-circle', 'home-circle-outline'],
+	Home: ['home-circle', 'home-circle-outline'],
+	Week: ['brightness-2', 'brightness-3'],
 };
 
-const screenOptions = ({route}: {route: RouteProp<ParamListBase, string>}) => {
-  return {
-    tabBarIcon: ({focused, color, size}: TabBarIconProps) => {
-      const {name} = route;
-      const focusedSize = focused ? size + 6 : size;
-      const focusedColor = focused ? Colors.lightBlue500 : color;
-      const [icon, iconOutline] = icons[name];
-      const iconName = focused ? icon : iconOutline;
-      return <Icon name={iconName} size={focusedSize} color={focusedColor} />;
-    },
-    headerShown: false,
-  };
+const screenOptions = ({
+	route,
+}: {
+	route: RouteProp<ParamListBase, string>;
+}) => {
+	return {
+		tabBarIcon: ({ focused, color, size }: TabBarIconProps) => {
+			const { name } = route;
+			const focusedSize = focused ? size + 6 : size;
+			const focusedColor = focused ? Colors.lightBlue500 : color;
+			const [icon, iconOutline] = icons[name];
+			const iconName = focused ? icon : iconOutline;
+			return <Icon name={iconName} size={focusedSize} color={focusedColor} />;
+		},
+		headerShown: false,
+	};
 };
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
-  return (
-    <Tab.Navigator screenOptions={screenOptions}>
-      <Tab.Screen
-        name="HomeNavigator"
-        component={HomeNavigator}
-        options={{tabBarLabel: 'Home', tabBarBadge: 3}}
-      />
-    </Tab.Navigator>
-  );
+	const interpolator = useNavigationHorizontalInterpolator();
+	const leftOptions = useMemo<StackNavigationOptions>(
+		() => ({
+			gestureDirection: 'horizontal-inverted',
+			cardStyleInterpolator: interpolator,
+		}),
+		[]
+	);
+	const rightOptions = useMemo<StackNavigationOptions>(
+		() => ({
+			gestureDirection: 'horizontal',
+			cardStyleInterpolator: interpolator,
+		}),
+		[]
+	);
+	return (
+		<Tab.Navigator screenOptions={screenOptions}>
+			<Tab.Screen name="Home" component={Home} />
+			<Tab.Screen name="Week" component={Week} />
+		</Tab.Navigator>
+	);
 }
