811c2bcec30a1f9966a96aa030c793913b9dd3e5
ww8007 <wshmin@naver.com>
2021-09-01 04:41:41 +0900

    Type : add login types

--- src/interface/login.ts
+++ src/interface/login.ts
@@ -1,11 +1,27 @@
 export interface Login {
 	id: string;
 	pw: string;
-	token: string;
+	cookies: string;
 	loginError: string;
+	thisYear: string;
+	semester: string;
+	dateList: Array<any>;
+	isSuccess: Array<any>;
+	data: any;
+	name: string;
 }
 
 export interface User {
 	id: string;
 	pw: string;
+}
+
+export interface LoginResponse {
+	cookies: string;
+	name: string;
+	yy: string; // 이번 년도
+	tmGbn: string; // 학기
+	outStayFrDt: Array<any>; // 외박 신청 시작일
+	outStayToDt: Array<any>; // 외박 신청 종료일
+	outStayStGbn: Array<any>; // 성공, 실패 여부
 }
