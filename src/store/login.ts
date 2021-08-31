27351599488d356ef17378a3c557592377e53987
ww8007 <wshmin@naver.com>
2021-09-01 04:46:34 +0900

    Feat : add initalLogin things, logout slice

--- src/store/login.ts
+++ src/store/login.ts
@@ -3,13 +3,19 @@ import { createRequestSaga } from '../hooks';
 import * as api from '../lib/api/login';
 import { takeLatest } from 'redux-saga/effects';
 import { createAction } from 'redux-actions';
-import type { Login, User } from '../interface/login';
+import type { Login, User, LoginResponse } from '../interface/login';
 
 const initialState: Login = {
 	id: '',
 	pw: '',
-	token: '',
+	cookies: '',
 	loginError: '',
+	dateList: [],
+	isSuccess: [],
+	semester: '',
+	thisYear: '',
+	data: {},
+	name: '',
 };
 
 const GET_LOGIN = 'login/GET_LOGIN';
@@ -27,16 +33,32 @@ export const loginSlice = createSlice({
 	initialState,
 	reducers: {
 		initialLogin: (state) => {
+			state.loginError = '';
+			state.cookies = '';
+			state.name = '';
+			state.dateList = [];
+			state.isSuccess = [];
+			state.loginError = '';
+		},
+		logoutHome: (state) => {
 			state.id = '';
 			state.pw = '';
 			state.loginError = '';
-			state.token = '';
+			state.cookies = '';
+			state.name = '';
+			state.dateList = [];
+			state.isSuccess = [];
+			state.loginError = '';
 		},
-		GET_LOGIN_SUCCESS: (state, action: PayloadAction<string>) => {
-			state.token = action.payload;
+		GET_LOGIN_SUCCESS: (state, action: PayloadAction<LoginResponse>) => {
+			state.cookies = action.payload.cookies;
+			state.name = action.payload.name;
+			state.isSuccess = action.payload.outStayStGbn;
+			state.dateList = action.payload.outStayToDt;
+			state.thisYear = action.payload.yy;
 		},
 		GET_LOGIN_FAILURE: (state, action: PayloadAction<any>) => {
-			state.loginError = 'fail';
+			state.loginError = action.payload;
 		},
 		setIdPw: (state, action: PayloadAction<User>) => {
 			state.id = action.payload.id;
@@ -46,7 +68,12 @@ export const loginSlice = createSlice({
 	extraReducers: {},
 });
 
-export const { GET_LOGIN_SUCCESS, GET_LOGIN_FAILURE, initialLogin, setIdPw } =
-	loginSlice.actions;
+export const {
+	GET_LOGIN_SUCCESS,
+	GET_LOGIN_FAILURE,
+	initialLogin,
+	setIdPw,
+	logoutHome,
+} = loginSlice.actions;
 
 export default loginSlice.reducer;
