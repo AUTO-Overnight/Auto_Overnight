e93c447d4c4f1c72a01b392c8223acdc44d8edb0
ww8007 <wshmin@naver.com>
2021-09-01 04:40:54 +0900

    Design : fix calendar color

--- src/hooks/useCalendarTheme.ts
+++ src/hooks/useCalendarTheme.ts
@@ -11,17 +11,18 @@ export const useCalendarTheme = (): {
 	const [theme, setTheme] = useState<CalendarTheme>({
 		backgroundColor: isDark ? Colors.grey900 : Colors.white, // 뒷배경
 		calendarBackground: isDark ? Colors.grey900 : Colors.white, // 뒷배경
-		textSectionTitleColor: isDark ? Colors.blueA700 : Colors.blueA700, // 요일 색상
+		textSectionTitleColor: isDark ? Colors.white : Colors.black, // 요일 색상
 		// textSectionTitleDisabledColor: Colors.red800,
 		// selectedDayBackgroundColor: Colors.red800,
 		// selectedDayTextColor: Colors.red800,
 		dayTextColor: isDark ? Colors.white : Colors.black,
-		textDisabledColor: isDark ? Colors.blueA700 : Colors.green100,
+		textDisabledColor: Colors.grey700,
 		monthTextColor: isDark ? Colors.white : Colors.black,
 		// indicatorColor: Colors.black,
 		textDayFontSize: 18,
 		textMonthFontSize: 18,
 		textDayHeaderFontSize: 18,
+		todayTextColor: isDark ? Colors.yellowA400 : Colors.blue600,
 	});
 
 	useEffect(() => {
@@ -32,9 +33,9 @@ export const useCalendarTheme = (): {
 				calendarBackground: Colors.white, // 뒷배경
 				dayTextColor: Colors.black,
 				monthTextColor: Colors.black,
-				indicatorColor: Colors.blue200,
-				textSectionTitleColor: Colors.blue600, // 요일 색상
-				textDisabledColor: Colors.blueA400,
+				textSectionTitleColor: Colors.black, // 요일 색상
+				textDisabledColor: Colors.grey700,
+				todayTextColor: Colors.blue600,
 			}));
 			console.log('다크 모드야');
 		} else {
@@ -44,9 +45,9 @@ export const useCalendarTheme = (): {
 				calendarBackground: Colors.grey900, // 뒷배경
 				dayTextColor: Colors.white,
 				monthTextColor: Colors.white,
-				indicatorColor: Colors.black,
-				textSectionTitleColor: Colors.blueA700, // 요일 색상
-				textDisabledColor: Colors.blueA700,
+				textSectionTitleColor: Colors.white, // 요일 색상
+				textDisabledColor: Colors.grey700,
+				todayTextColor: Colors.yellowA400,
 			}));
 			console.log('다크 모드 아냐');
 		}
