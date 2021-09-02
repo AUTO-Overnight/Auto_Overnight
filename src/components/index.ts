7ab05acc4e21e641315000a4ccc99a86484464c6
ww8007 <wshmin@naver.com>
2021-09-03 05:17:53 +0900

    Feat : add export useModal, BonusTable, data...

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export * from './TouchableView';
 export * from './Avatar';
 export * from './IconText';
-
+export * from './useModal';
 export * from './LeftSwipe';
 export * from './LeftRightNavigation';
+export * from './BonusTable';
+export * from './dataViewToScreenSize';
