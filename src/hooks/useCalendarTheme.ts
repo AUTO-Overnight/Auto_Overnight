edd9d510def466b1cf1be502ea2e6b38f8f8e216
ww8007 <wshmin@naver.com>
2021-09-04 23:44:13 +0900

    Design : fix dark, default theme

--- src/hooks/useCalendarTheme.ts
+++ src/hooks/useCalendarTheme.ts
@@ -3,14 +3,15 @@ import { useEffect, useState } from 'react';
 import { Colors } from 'react-native-paper';
 import { useTheme } from '@react-navigation/native';
 import { useToggleTheme } from '../contexts';
+import { Col } from 'react-native-table-component';
 export const useCalendarTheme = (): {
 	theme: CalendarTheme;
 	isDark: boolean;
 } => {
 	const isDark = useTheme().dark;
 	const [theme, setTheme] = useState<CalendarTheme>({
-		backgroundColor: isDark ? Colors.grey900 : Colors.white, // 뒷배경
-		calendarBackground: isDark ? Colors.grey900 : Colors.white, // 뒷배경
+		backgroundColor: isDark ? '#222831' : Colors.white, // 뒷배경
+		calendarBackground: isDark ? '#222831' : Colors.white, // 뒷배경
 		textSectionTitleColor: isDark ? Colors.white : Colors.black, // 요일 색상
 		// textSectionTitleDisabledColor: Colors.red800,
 		// selectedDayBackgroundColor: Colors.red800,
@@ -20,9 +21,10 @@ export const useCalendarTheme = (): {
 		monthTextColor: isDark ? Colors.white : Colors.black,
 		// indicatorColor: Colors.black,
 		textDayFontSize: 18,
+		arrowColor: isDark ? Colors.yellow900 : '#FFAAA7',
 		textMonthFontSize: 18,
 		textDayHeaderFontSize: 18,
-		todayTextColor: isDark ? Colors.yellowA400 : Colors.blue600,
+		todayTextColor: isDark ? Colors.yellow900 : Colors.red500,
 	});
 
 	useEffect(() => {
@@ -35,19 +37,26 @@ export const useCalendarTheme = (): {
 				monthTextColor: Colors.black,
 				textSectionTitleColor: Colors.black, // 요일 색상
 				textDisabledColor: Colors.grey700,
-				todayTextColor: Colors.blue600,
+				todayTextColor: Colors.red500,
+				textDayFontSize: 18,
+				arrowColor: '#345B63',
+				textMonthFontSize: 18,
+				textDayHeaderFontSize: 18,
 			}));
 			console.log('다크 모드야');
 		} else {
 			setTheme((prevState) => ({
 				...prevState,
-				backgroundColor: Colors.green900,
-				calendarBackground: Colors.grey900, // 뒷배경
+				backgroundColor: '#222831',
+				calendarBackground: '#222831', // 뒷배경
 				dayTextColor: Colors.white,
 				monthTextColor: Colors.white,
 				textSectionTitleColor: Colors.white, // 요일 색상
 				textDisabledColor: Colors.grey700,
 				todayTextColor: Colors.yellowA400,
+				textDayFontSize: 18,
+				textMonthFontSize: 18,
+				textDayHeaderFontSize: 18,
 			}));
 			console.log('다크 모드 아냐');
 		}
