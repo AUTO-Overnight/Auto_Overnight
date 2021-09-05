e810a0e3739543c9eb23e6d8cb5f7e1e20f101d2
ww8007 <wshmin@naver.com>
2021-09-05 23:19:24 +0900

    Feat : add switch toggle dark, message to develope

--- src/screens/DrawerContent.tsx
+++ src/screens/DrawerContent.tsx
@@ -8,7 +8,7 @@ import type { FC } from 'react';
 import type { DrawerContentComponentProps } from '@react-navigation/drawer';
 import { DrawerContentScrollView } from '@react-navigation/drawer';
 import { DrawerActions } from '@react-navigation/native';
-import { Avatar } from '../components';
+import { Avatar, TouchableView } from '../components';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
 
@@ -28,6 +28,10 @@ const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
 			<NavigationHeader
 				Right={() => <Icon name="close" size={24} onPress={close} />}
 			/>
+			<View style={[styles.row]}>
+				<Switch style={[styles.row, { marginLeft: 10 }]} />
+				<Text style={styles.switchText}>Default / Dark </Text>
+			</View>
 			<View style={[styles.content]}>
 				<View style={[styles.row]}>
 					{/* <Avatar uri={loginUser.avatar} size={40} /> */}
@@ -39,12 +43,16 @@ const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
 						ellipsizeMode="tail"
 						style={[styles.text, styles.m]}
 					>
-						{id}
+						ID : {id}
 					</Text>
 				</View>
-				<View style={[styles.row, { marginTop: 20 }]}>
-					<Switch />
-				</View>
+				<View style={styles.line} />
+				<TouchableView
+					style={[styles.touchableView, { backgroundColor: 'white' }]}
+				>
+					<Text style={styles.text}>문의/건의 사항</Text>
+					{/* <Icon name="login" size={24}></Icon> */}
+				</TouchableView>
 			</View>
 		</DrawerContentScrollView>
 	);
@@ -52,8 +60,27 @@ const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
 export default DrawerContent;
 const styles = StyleSheet.create({
 	view: { flex: 1, padding: 5 },
-	row: { flexDirection: 'row', padding: 5, alignItems: 'center' },
+	row: { flexDirection: 'row', padding: 10, alignItems: 'center' },
+	col: { flexDirection: 'column', padding: 10, alignItems: 'center' },
 	m: { marginLeft: 5 },
 	text: { fontSize: 20 },
+	touchableView: {
+		marginTop: 10,
+		flexDirection: 'row',
+		height: 50,
+		borderRadius: 10,
+		width: '100%',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 	content: { flex: 1, padding: 5 },
+	line: {
+		marginTop: 10,
+		borderBottomColor: 'white',
+		borderBottomWidth: 3,
+	},
+	switchText: {
+		fontSize: 20,
+		marginLeft: 5,
+	},
 });
