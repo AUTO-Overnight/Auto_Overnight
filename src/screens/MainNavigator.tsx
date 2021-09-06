61e86ded294a77dc5fd1bea583681e5fa0b5f596
ww8007 <wshmin@naver.com>
2021-09-07 03:32:18 +0900

    Feat : add useEffect for navigate with persist

--- src/screens/MainNavigator.tsx
+++ src/screens/MainNavigator.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Login from './Login';
 import TabNavigator from './TabNavigator';
 import DrawerContent from './DrawerContent';
+import { Colors } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/core';
 
+import { useSelector } from 'react-redux';
+import type { RootState } from '../store';
 const Drawer = createDrawerNavigator();
 
 export default function MainNavigator() {
+	const { name, rememberID } = useSelector(
+		({ calendar, login }: RootState) => ({
+			name: login.name,
+			rememberID: login.rememberID,
+		})
+	);
+	const navigation = useNavigation();
+	useEffect(() => {
+		if (name) {
+			console.log(rememberID);
+			console.log(name);
+			navigation.navigate('TabNavigator');
+		}
+	}, [name]);
 	return (
 		<Drawer.Navigator
-			screenOptions={{ headerShown: false }}
+			screenOptions={{
+				headerShown: false,
+			}}
 			drawerContent={(props) => <DrawerContent {...props} />}
 		>
 			<Drawer.Screen name="Login" component={Login} />
