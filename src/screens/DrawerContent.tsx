74a24dde3d242c3ba8f677fa757bf7400ac064f8
ww8007 <wshmin@naver.com>
2021-09-08 04:21:40 +0900

    Feat : onClickForm uscallback function Design : fix color, font

--- src/screens/DrawerContent.tsx
+++ src/screens/DrawerContent.tsx
@@ -1,22 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useMemo, useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { StyleSheet, Linking } from 'react-native';
 // prettier-ignore
-import {View, Text, NavigationHeader, UnderlineText,
+import {View, Text, NavigationHeader,
 MaterialCommunityIcon as Icon, Switch} from '../theme';
 import type { FC } from 'react';
 import type { DrawerContentComponentProps } from '@react-navigation/drawer';
 import { DrawerContentScrollView } from '@react-navigation/drawer';
 import { DrawerActions, useTheme } from '@react-navigation/native';
 import { Avatar, TouchableView } from '../components';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../store';
 import { Colors } from 'react-native-paper';
+import { initialLogin } from '../store/login';
+import { getLogin } from '../store/login';
+import { logoutInitial } from '../store/calendar';
+import { blue200 } from 'react-native-paper/lib/typescript/styles/colors';
 const backWhiteColor = '#FFFF';
 const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
-	const { name, id } = useSelector(({ login }: RootState) => ({
+	const { name, id, pw, rememberID } = useSelector(({ login }: RootState) => ({
 		name: login.name,
+		rememberID: login.rememberID,
 		id: login.id,
+		pw: login.pw,
 	}));
 	const { navigation } = props;
 	const close = useCallback(
@@ -24,10 +30,24 @@ const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
 		[]
 	);
 	const isDark = useTheme().dark;
+	const dispatch = useDispatch();
 	const onClickOpenChat = useCallback(() => {
 		Linking.openURL('https://open.kakao.com/o/sA4uughd');
 	}, []);
-	const onReload = useCallback(() => {}, []);
+	const onClickForm = useCallback(() => {
+		Linking.openURL(
+			'https://the-form.io/forms/survey/response/7ee611fa-597c-40e6-80b6-d0e905ea2781'
+		);
+	}, []);
+	const onPressReload = useCallback(() => {
+		dispatch(initialLogin());
+		dispatch(logoutInitial());
+		const user = {
+			userId: id,
+			userPw: pw,
+		};
+		dispatch(getLogin(user));
+	}, [id, pw]);
 	return (
 		<DrawerContentScrollView
 			{...props}
@@ -37,7 +57,7 @@ const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
 			]}
 		>
 			<NavigationHeader
-				Right={() => <Icon name="close" size={30} onPress={close} />}
+				Right={() => <Icon name="close" size={32} onPress={close} />}
 			/>
 			<View style={{ backgroundColor: isDark ? Colors.black : backWhiteColor }}>
 				<View
@@ -52,7 +72,14 @@ const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
 							{ backgroundColor: isDark ? Colors.black : backWhiteColor },
 						]}
 					>
-						<Text style={styles.switchText}>Default / Dark </Text>
+						<Text
+							style={[
+								styles.switchText,
+								{ color: isDark ? Colors.white : Colors.grey800 },
+							]}
+						>
+							Default / Dark
+						</Text>
 						<Switch style={[styles.row, { marginLeft: 10 }]} />
 					</View>
 				</View>
@@ -69,7 +96,15 @@ const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
 						]}
 					>
 						{/* <Avatar uri={loginUser.avatar} size={40} /> */}
-						<Text style={[styles.text, styles.m]}>안녕하세요 {name}님</Text>
+						<Text
+							style={[
+								styles.text,
+								styles.m,
+								{ color: isDark ? Colors.white : Colors.grey800 },
+							]}
+						>
+							안녕하세요 {name}님
+						</Text>
 					</View>
 					<View
 						style={[
@@ -80,54 +115,126 @@ const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
 						<Text
 							numberOfLines={1}
 							ellipsizeMode="tail"
-							style={[styles.text, styles.m]}
+							style={[
+								styles.text,
+								styles.m,
+								{ color: isDark ? Colors.white : Colors.grey800 },
+							]}
 						>
 							ID : {id}
 						</Text>
 					</View>
-
-					<View
-						style={[
-							styles.line,
-							{ borderBottomColor: isDark ? 'white' : Colors.black },
-						]}
-					/>
 					<View
 						style={[
 							styles.rowCircle,
-							{ backgroundColor: isDark ? '#152D35' : backWhiteColor },
+							{
+								alignSelf: 'center',
+								backgroundColor: isDark ? '#3b6a7a' : Colors.blue100,
+								width: '90%',
+								height: 50,
+								borderRadius: 10,
+								marginTop: 50,
+							},
 						]}
 					>
 						<View
-							style={[styles.circle, { backgroundColor: Colors.yellow500 }]}
+							style={[
+								styles.circle,
+								{ backgroundColor: isDark ? Colors.yellow500 : Colors.red500 },
+							]}
 						/>
-						<Text style={styles.text}>승인</Text>
-						<View style={[styles.circle, { backgroundColor: Colors.red500 }]} />
-						<Text style={styles.text}>미승인</Text>
+						<Text
+							style={[
+								styles.touchText,
+								{ color: isDark ? Colors.white : Colors.grey800 },
+							]}
+						>
+							승인
+						</Text>
+						<View
+							style={[
+								styles.circle,
+								{ backgroundColor: isDark ? Colors.red500 : Colors.blue500 },
+							]}
+						/>
+						<Text
+							style={[
+								styles.touchText,
+								{ color: isDark ? Colors.white : Colors.grey800 },
+							]}
+						>
+							미승인
+						</Text>
 					</View>
-					<View
+					{/* <View
 						style={[
 							styles.line,
 							{ borderBottomColor: isDark ? 'white' : Colors.black },
 						]}
-					/>
+					/> */}
 					<TouchableView
-						onPress={onClickOpenChat}
+						onPress={onPressReload}
 						style={[
 							styles.touchableView,
-							{ backgroundColor: isDark ? '#152D35' : Colors.red300 },
+							{ backgroundColor: isDark ? '#295868' : Colors.blue200 },
 						]}
 					>
-						<Text style={styles.buttonText}>문의/건의 사항</Text>
+						<Text
+							style={[
+								styles.buttonText,
+								{ color: isDark ? Colors.white : Colors.grey800 },
+							]}
+						>
+							새로고침
+						</Text>
 					</TouchableView>
 					<TouchableView
 						onPress={onClickOpenChat}
 						style={[
 							styles.touchableView,
-							{ backgroundColor: isDark ? '#152D35' : Colors.red300 },
+							{ backgroundColor: isDark ? '#214653' : Colors.blue300 },
 						]}
 					>
-						<Text style={styles.buttonText}>설문조사</Text>
+						<Text
+							style={[
+								styles.buttonText,
+								{ color: isDark ? Colors.white : Colors.grey800 },
+							]}
+						>
+							문의사항
+						</Text>
+					</TouchableView>
+					<TouchableView
+						onPress={onClickForm}
+						style={[
+							styles.touchableView,
+							{ backgroundColor: isDark ? '#152D35' : Colors.blue400 },
+						]}
+					>
+						<Text
+							style={[
+								styles.buttonText,
+								{ color: isDark ? Colors.white : Colors.grey800 },
+							]}
+						>
+							설문조사
+						</Text>
+					</TouchableView>
+					<TouchableView
+						onPress={onClickForm}
+						style={[
+							styles.touchableView,
+							{ backgroundColor: isDark ? '#142328' : Colors.blue500 },
+						]}
+					>
+						<Text
+							style={[
+								styles.buttonText,
+								{ color: isDark ? Colors.white : Colors.grey800 },
+							]}
+						>
+							버그 리포트
+						</Text>
 					</TouchableView>
 				</View>
 			</View>
@@ -150,21 +257,22 @@ const styles = StyleSheet.create({
 		justifyContent: 'space-between',
 	},
 	col: { flexDirection: 'column', padding: 10, alignItems: 'center' },
-	m: { marginLeft: 5 },
-	text: { fontSize: 20 },
+	m: { marginLeft: 5, marginTop: 10 },
+	text: { fontSize: 19 },
+	touchText: { fontSize: 17 },
 	buttonText: {
-		fontSize: 20,
+		fontSize: 17,
 		color: Colors.white,
 	},
 	touchableView: {
-		marginTop: 20,
+		marginTop: 40,
 		flexDirection: 'column',
 		height: 50,
-		marginLeft: 5,
 		borderRadius: 10,
-		width: '95%',
+		width: '90%',
 		justifyContent: 'center',
 		alignItems: 'center',
+		alignSelf: 'center',
 	},
 	content: { padding: 5 },
 	line: {
