014b40d376e9f8b6284476c7ccb9bd8bb66faf97
ww8007 <wshmin@naver.com>
2021-09-11 19:28:49 +0900

    Feat: add useIsDarkMode custom hook use for refactor other components

---
+++ src/hooks/useIsDarkMode.ts
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useTheme } from '@react-navigation/native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+export function useIsDarkMode(): {
+	isDark: boolean;
+	backgroundColor: string;
+	textColor: string;
+} {
+	const isDark = useTheme().dark;
+	const backgroundColor = isDark ? Colors.black : Colors.white;
+	const textColor = isDark ? Colors.white : Colors.grey800;
+
+	return { isDark, backgroundColor, textColor };
+}
