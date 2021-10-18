c4895c82b789de4808db5c3ed36e395953079633
ww8007 <wshmin@naver.com>
2021-10-18 23:17:09 +0900

    Feat: add weather, weatherSaga  root[Reducer/Saga]

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,21 +6,23 @@ import calendar, { calendarSaga } from './calendar';
 import loading from './loading';
 import login, { loginSaga } from './login';
 import bonus, { bonusSaga } from './bonus';
+import weather, { weatherSaga } from './weather';
 // import storageSession from 'redux-persist/lib/storage/session';
 const persistConfig = {
 	key: 'root',
 	storage: AsyncStorage,
-	blacklist: ['calendar', 'bonus'],
+	blacklist: ['calendar', 'bonus', 'weather'],
 };
 const rootReducer = combineReducers({
 	login,
 	loading,
 	calendar,
 	bonus,
+	weather,
 });
 export default persistReducer(persistConfig, rootReducer);
 export type RootState = ReturnType<typeof rootReducer>;
 
 export function* rootSaga() {
-	yield all([loginSaga(), calendarSaga(), bonusSaga()]);
+	yield all([loginSaga(), calendarSaga(), bonusSaga(), weatherSaga()]);
 }
