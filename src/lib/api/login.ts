5ff8db44db1ea2de5aeef854891820dd1304d48e
ww8007 <wshmin@naver.com>
2021-10-27 06:09:07 +0900

    Feat: add getVersion API

--- src/lib/api/login.ts
+++ src/lib/api/login.ts
@@ -1,5 +1,7 @@
 import client from './client';
+import axios from 'axios';
 import type { User } from '../../interface';
+import { VERSION_URL } from '@env';
 export const getLogin = ({ userId, userPw }: User) => {
 	userId = encodeURIComponent(userId);
 	userPw = encodeURIComponent(userPw);
@@ -8,4 +10,8 @@ export const getLogin = ({ userId, userPw }: User) => {
 		password: userPw,
 	});
 	return client.post('/login', user);
+};
+
+export const getVersion = () => {
+	return axios.get(VERSION_URL);
 };
