d5a1bc45b3e96e14f4c3e663c0bf77c77f0fca7d
ww8007 <wshmin@naver.com>
2022-03-01 23:31:02 +0900

    Refactor: render with useState value Design: fix order, design

--- src/screens/DrawerContent.tsx
+++ src/screens/DrawerContent.tsx
@@ -1,6 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useCallback, useState } from 'react';
-import { StyleSheet, Linking } from 'react-native';
+import {
+	StyleSheet,
+	Linking,
+	TouchableHighlight,
+	Dimensions
+} from 'react-native';
 // prettier-ignore
 import {View, Text, NavigationHeader,
 MaterialCommunityIcon as Icon, Switch} from '../theme';
@@ -19,14 +24,18 @@ import { blue200 } from 'react-native-paper/lib/typescript/styles/colors';
 import { useModal } from '../components';
 import Icons from 'react-native-vector-icons/AntDesign';
 import { BusModal } from '../components';
+import Font5Icon from 'react-native-vector-icons/FontAwesome5';
+import Ionic from 'react-native-vector-icons/Ionicons';
+import { StatusBar } from 'expo-status-bar';
 const backWhiteColor = '#FFFF';
 const fontSize = 15;
+const screen = Dimensions.get('screen');
 const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
 	const { name, id, pw, rememberID } = useSelector(({ login }: RootState) => ({
-		name: login.name,
+		name: login.loginState.name,
 		rememberID: login.rememberID,
 		id: login.id,
-		pw: login.pw,
+		pw: login.pw
 	}));
 	const { navigation } = props;
 	const close = useCallback(
@@ -58,7 +67,7 @@ const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
 		dispatch(logoutInitial());
 		const user = {
 			userId: id,
-			userPw: pw,
+			userPw: pw
 		};
 		dispatch(getLogin(user));
 	}, [id, pw]);
@@ -67,8 +76,9 @@ const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
 	const title = '[업데이트 내역]\n 1.0.4';
 	const { modalVisible, ModalView, setModalVisible } = useModal({
 		text,
-		title,
+		title
 	});
+
 	const [busModalVisible, setBusModalVisible] = useState(false);
 	const [mode, setMode] = useState('2');
 	const onPressUpdate = useCallback(() => {
@@ -77,381 +87,175 @@ const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
 		);
 		setModalVisible(true);
 	}, []);
+	const [buttons] = useState([
+		{
+			onPress: onClickCallOne,
+			text: '1 기숙사',
+			iconName: 'ios-phone-portrait-outline',
+			iconColor: Colors.blue500
+		},
+		{
+			onPress: onClickCallTwo,
+			text: '2 기숙사',
+			iconName: 'ios-phone-portrait-outline',
+			iconColor: Colors.blue500
+		}
+	]);
+	const [secondButtons] = useState([
+		{
+			onPress: onClickForm,
+			text: '설문조사',
+			iconName: 'ios-checkmark-sharp',
+			iconColor: Colors.blue500
+		},
+		{
+			onPress: onClickOpenChat,
+			text: '카카오톡',
+			iconName: 'chatbubble-sharp',
+			iconColor: Colors.blue500
+		}
+	]);
+	const [thirdButtons] = useState([
+		{
+			onPress: onClickBusPDF,
+			text: '셔틀 시간표 보기',
+			iconName: 'md-bus-sharp',
+			iconColor: Colors.blue500
+		},
+		{
+			onPress: onPressUpdate,
+			text: '업데이트 내역',
+			iconName: 'cloud-download',
+			iconColor: Colors.blue500
+		}
+	]);
 	return (
 		<DrawerContentScrollView
 			{...props}
 			contentContainerStyle={[
 				styles.view,
-				{ backgroundColor: isDark ? Colors.black : backWhiteColor },
+				{ backgroundColor: isDark ? Colors.black : '#fff' }
 			]}
 		>
+			<StatusBar style={isDark ? 'light' : 'dark'} />
 			<NavigationHeader
-				Left={() => <Switch style={[styles.row, { marginLeft: 23 }]} />}
+				backgroundColor={isDark ? 'black' : '#fff'}
+				// Left={() => <Switch style={[styles.row, { marginLeft: 23 }]} />}
 				Right={() => <Icon name="close" size={32} onPress={close} />}
 			/>
 			<View style={{ backgroundColor: isDark ? Colors.black : backWhiteColor }}>
 				<View
 					style={[
 						styles.content,
-						{ backgroundColor: isDark ? Colors.black : backWhiteColor },
+						{ backgroundColor: isDark ? Colors.black : backWhiteColor }
 					]}
 				>
-					<View
-						style={[
-							styles.row,
-							{ backgroundColor: isDark ? Colors.black : backWhiteColor },
-						]}
-					>
-						{/* <Avatar uri={loginUser.avatar} size={40} /> */}
-						<Text
-							style={[
-								styles.text,
-								styles.m,
-								{ color: isDark ? Colors.white : Colors.grey800 },
-							]}
-						>
-							안녕하세요 {name}님
-						</Text>
-					</View>
-					<View
-						style={[
-							styles.row,
-							{ backgroundColor: isDark ? Colors.black : backWhiteColor },
-						]}
-					>
-						<Text
-							numberOfLines={1}
-							ellipsizeMode="tail"
-							style={[
-								styles.text,
-								styles.m,
-								{ color: isDark ? Colors.white : Colors.grey800 },
-							]}
-						>
-							ID : {id}
-						</Text>
-					</View>
-					<View
-						style={[
-							styles.rowCircle,
-							{
-								alignSelf: 'center',
-								backgroundColor: isDark ? Colors.black : Colors.white,
-								width: '90%',
-								height: 50,
-								borderRadius: 10,
-								// marginTop: 30,
-								// marginBottom: 10,
-							},
-						]}
-					>
-						<View
-							style={[
-								styles.circle,
-								{ backgroundColor: isDark ? Colors.yellow500 : Colors.red500 },
-							]}
-						/>
-						<Text
-							style={[
-								styles.touchText,
-								{ color: isDark ? Colors.white : Colors.grey800 },
-							]}
-						>
-							승인
-						</Text>
-						<View
-							style={[
-								styles.circle,
-								{ backgroundColor: isDark ? Colors.red500 : Colors.blue500 },
-							]}
-						/>
-						<Text
-							style={[
-								styles.touchText,
-								{ color: isDark ? Colors.white : Colors.grey800 },
-							]}
-						>
-							미승인
-						</Text>
-					</View>
-					<View
-						style={[
-							styles.rowButton,
-							{ backgroundColor: isDark ? Colors.black : Colors.white },
-						]}
-					>
-						<TouchableView
-							onPress={onClickCallOne}
-							style={[
-								styles.touchableView,
-								// { backgroundColor: isDark ? '#295868' : Colors.blue200 },
-							]}
-						>
-							<Text
-								style={[
-									styles.buttonText,
-									{ color: isDark ? Colors.white : Colors.grey800 },
-								]}
+					<Text style={[styles.titleText, { marginTop: 0 }]}>
+						기숙사 전화 연결
+					</Text>
+					<View style={[styles.blankView]} />
+					<View style={styles.columnView}>
+						{buttons.map((button) => (
+							<TouchableHighlight
+								activeOpacity={0.5}
+								underlayColor={isDark ? Colors.grey800 : Colors.grey200}
+								key={button.text}
+								style={{ backgroundColor: isDark ? 'black' : '#fff' }}
+								onPress={() => button.onPress()}
 							>
-								{'      '}1 기숙사 사감실 전화 연결
-							</Text>
-						</TouchableView>
-						<Icons
-							name="phone"
-							style={[
-								styles.buttonText,
-								styles.iconStyle,
-								{
-									color: isDark ? Colors.white : Colors.grey800,
-								},
-							]}
-						/>
+								<View style={styles.rowView}>
+									<Ionic
+										name={button.iconName}
+										size={21}
+										style={styles.iconStyle}
+										color={button.iconColor}
+									/>
+									<Text style={styles.touchText}>{button.text}</Text>
+									<View style={styles.iconView}>
+										<Font5Icon
+											name="angle-right"
+											size={19}
+											color={isDark ? 'white' : 'black'}
+											style={styles.rightIconStyle}
+										/>
+									</View>
+								</View>
+							</TouchableHighlight>
+						))}
 					</View>
 					<View
 						style={[
-							styles.rowButton,
-							{ backgroundColor: isDark ? Colors.black : Colors.white },
+							styles.blankGreyView,
+							{ backgroundColor: isDark ? Colors.grey800 : Colors.grey300 }
 						]}
-					>
-						<TouchableView
-							onPress={onClickCallTwo}
-							style={[
-								styles.touchableView,
-								// { backgroundColor: isDark ? '#295868' : Colors.blue200 },
-							]}
-						>
-							<Text
-								style={[
-									styles.buttonText,
-									{ color: isDark ? Colors.white : Colors.grey800 },
-								]}
+					/>
+					<Text style={styles.titleText}>문의하기</Text>
+					<View style={styles.blankView} />
+					<View style={styles.columnView}>
+						{secondButtons.map((button) => (
+							<TouchableHighlight
+								activeOpacity={0.5}
+								underlayColor={isDark ? Colors.grey800 : Colors.grey200}
+								key={button.text}
+								style={{ backgroundColor: isDark ? 'black' : '#fff' }}
+								onPress={() => button.onPress()}
 							>
-								{'      '}2 기숙사 사감실 전화 연결
-							</Text>
-						</TouchableView>
-						<Icons
-							name="phone"
-							style={[
-								styles.buttonText,
-								styles.iconStyle,
-								{
-									color: isDark ? Colors.white : Colors.grey800,
-								},
-							]}
-						/>
+								<View style={styles.rowView}>
+									<Ionic
+										name={button.iconName}
+										size={21}
+										style={styles.iconStyle}
+										color={button.iconColor}
+									/>
+									<Text style={styles.touchText}>{button.text}</Text>
+									<View style={styles.iconView}>
+										<Font5Icon
+											name="angle-right"
+											size={19}
+											color={isDark ? 'white' : 'black'}
+											style={styles.rightIconStyle}
+										/>
+									</View>
+								</View>
+							</TouchableHighlight>
+						))}
 					</View>
 					<View
 						style={[
-							styles.rowButton,
-							{ backgroundColor: isDark ? Colors.black : Colors.white },
+							styles.blankGreyView,
+							{ backgroundColor: isDark ? Colors.grey800 : Colors.grey300 }
 						]}
-					>
-						<TouchableView
-							activeOpacity={0.8}
-							onPress={() => setBusModalVisible(!busModalVisible)}
-							style={[
-								styles.touchableView,
-								// { backgroundColor: isDark ? '#142328' : Colors.blue500 },
-							]}
-						>
-							<Text
-								style={[
-									styles.buttonText,
-									{ color: isDark ? Colors.white : Colors.grey800 },
-								]}
+					/>
+					<Text style={styles.titleText}>Etc</Text>
+					<View style={styles.blankView} />
+					<View style={styles.columnView}>
+						{thirdButtons.map((button) => (
+							<TouchableHighlight
+								activeOpacity={0.5}
+								underlayColor={isDark ? Colors.grey800 : Colors.grey200}
+								key={button.text}
+								style={{ backgroundColor: isDark ? 'black' : '#fff' }}
+								onPress={() => button.onPress()}
 							>
-								{'      '}
-								최단 셔틀 시간 계산
-							</Text>
-
-							<ModalView />
-						</TouchableView>
-						<Icons
-							name="aliwangwang"
-							style={[
-								styles.buttonText,
-								styles.iconStyle,
-								{
-									color: isDark ? Colors.white : Colors.grey800,
-								},
-							]}
-						/>
-					</View>
-					<View
-						style={[
-							styles.rowButton,
-							{ backgroundColor: isDark ? Colors.black : Colors.white },
-						]}
-					>
-						<TouchableView
-							activeOpacity={0.8}
-							onPress={onClickBusPDF}
-							style={[
-								styles.touchableView,
-								// { backgroundColor: isDark ? '#142328' : Colors.blue500 },
-							]}
-						>
-							<Text
-								style={[
-									styles.buttonText,
-									{ color: isDark ? Colors.white : Colors.grey800 },
-								]}
-							>
-								{'      '}
-								셔틀 시간표 보기
-							</Text>
-
-							<ModalView />
-						</TouchableView>
-						<Icons
-							name="right"
-							style={[
-								styles.buttonText,
-								styles.iconStyle,
-								{
-									color: isDark ? Colors.white : Colors.grey800,
-								},
-							]}
-						/>
-					</View>
-					<View
-						style={[
-							styles.rowButton,
-							{ backgroundColor: isDark ? Colors.black : Colors.white },
-						]}
-					>
-						<TouchableView
-							onPress={onPressReload}
-							style={[
-								styles.touchableView,
-								// { backgroundColor: isDark ? '#295868' : Colors.blue200 },
-							]}
-						>
-							<Text
-								style={[
-									styles.buttonText,
-									{ color: isDark ? Colors.white : Colors.grey800 },
-								]}
-							>
-								{'      '}
-								새로고침
-							</Text>
-						</TouchableView>
-						<Icons
-							name="right"
-							style={[
-								styles.buttonText,
-								styles.iconStyle,
-								{
-									color: isDark ? Colors.white : Colors.grey800,
-								},
-							]}
-						/>
-					</View>
-					<View
-						style={[
-							styles.rowButton,
-							{ backgroundColor: isDark ? Colors.black : Colors.white },
-						]}
-					>
-						<TouchableView
-							onPress={onClickOpenChat}
-							style={[
-								styles.touchableView,
-								// { backgroundColor: isDark ? '#214653' : Colors.blue300 },
-							]}
-						>
-							<Text
-								style={[
-									styles.buttonText,
-									{ color: isDark ? Colors.white : Colors.grey800 },
-								]}
-							>
-								{'      '}
-								문의사항
-							</Text>
-						</TouchableView>
-						<Icons
-							name="right"
-							style={[
-								styles.buttonText,
-								styles.iconStyle,
-								{
-									color: isDark ? Colors.white : Colors.grey800,
-								},
-							]}
-						/>
-					</View>
-					<View
-						style={[
-							styles.rowButton,
-							{ backgroundColor: isDark ? Colors.black : Colors.white },
-						]}
-					>
-						<TouchableView
-							onPress={onClickForm}
-							style={[
-								styles.touchableView,
-								// { backgroundColor: isDark ? '#152D35' : Colors.blue400 },
-							]}
-						>
-							<Text
-								style={[
-									styles.buttonText,
-									{ color: isDark ? Colors.white : Colors.grey800 },
-								]}
-							>
-								{'      '}
-								설문조사
-							</Text>
-						</TouchableView>
-						<Icons
-							name="right"
-							style={[
-								styles.buttonText,
-								styles.iconStyle,
-								{
-									color: isDark ? Colors.white : Colors.grey800,
-								},
-							]}
-						/>
-					</View>
-					<View
-						style={[
-							styles.rowButton,
-							{ backgroundColor: isDark ? Colors.black : Colors.white },
-						]}
-					>
-						<TouchableView
-							activeOpacity={0.8}
-							onPress={onPressUpdate}
-							style={[
-								styles.touchableView,
-								// { backgroundColor: isDark ? '#142328' : Colors.blue500 },
-							]}
-						>
-							<Text
-								style={[
-									styles.buttonText,
-									{ color: isDark ? Colors.white : Colors.grey800 },
-								]}
-							>
-								{'      '}
-								업데이트 내역
-							</Text>
-
-							<ModalView />
-						</TouchableView>
-						<Icons
-							name="right"
-							style={[
-								styles.buttonText,
-								styles.iconStyle,
-								{
-									color: isDark ? Colors.white : Colors.grey800,
-								},
-							]}
-						/>
+								<View style={styles.rowView}>
+									<Ionic
+										name={button.iconName}
+										size={21}
+										style={styles.iconStyle}
+										color={button.iconColor}
+									/>
+									<Text style={styles.touchText}>{button.text}</Text>
+									<View style={styles.iconView}>
+										<Font5Icon
+											name="angle-right"
+											size={19}
+											color={isDark ? 'white' : 'black'}
+											style={styles.rightIconStyle}
+										/>
+									</View>
+								</View>
+							</TouchableHighlight>
+						))}
 					</View>
 					<BusModal
 						modalVisible={busModalVisible}
@@ -460,6 +264,55 @@ const DrawerContent: FC<DrawerContentComponentProps> = (props) => {
 						// mode={mode}
 						// setMode={setMode}
 					/>
+					<View
+						style={[
+							styles.blankGreyView,
+							{ backgroundColor: isDark ? Colors.grey800 : Colors.grey300 }
+						]}
+					/>
+					<Text style={styles.titleText}>달력 정보</Text>
+					<View style={styles.blankView} />
+					<View
+						style={[
+							styles.rowCircle,
+							{
+								alignSelf: 'center',
+								backgroundColor: isDark ? Colors.black : Colors.white,
+								width: '90%',
+								height: 50,
+								borderRadius: 10
+							}
+						]}
+					>
+						<View
+							style={[
+								styles.circle,
+								{ backgroundColor: isDark ? Colors.yellow500 : Colors.red500 }
+							]}
+						/>
+						<Text
+							style={[
+								styles.touchText,
+								{ color: isDark ? Colors.white : Colors.grey800 }
+							]}
+						>
+							승인
+						</Text>
+						<View
+							style={[
+								styles.circle,
+								{ backgroundColor: isDark ? Colors.red500 : Colors.blue500 }
+							]}
+						/>
+						<Text
+							style={[
+								styles.touchText,
+								{ color: isDark ? Colors.white : Colors.grey800 }
+							]}
+						>
+							미승인
+						</Text>
+					</View>
 				</View>
 			</View>
 		</DrawerContentScrollView>
@@ -472,31 +325,36 @@ const styles = StyleSheet.create({
 		flexDirection: 'row',
 		padding: 0,
 		justifyContent: 'center',
-		alignItems: 'center',
+		alignItems: 'center'
 	},
 	row: {
 		flexDirection: 'row',
 		padding: 15,
 		alignItems: 'center',
-		justifyContent: 'flex-start',
+		justifyContent: 'flex-start'
 	},
 	rowCircle: {
 		flexDirection: 'row',
 		padding: 15,
 		alignItems: 'center',
-		justifyContent: 'space-between',
+		justifyContent: 'space-between'
 	},
 	col: { flexDirection: 'column', padding: 10, alignItems: 'center' },
 	m: { marginLeft: 10, marginTop: 10 },
-	text: { fontSize: fontSize },
-	touchText: { fontSize: fontSize },
+	text: { fontSize: fontSize, fontFamily: 'NanumSquareR' },
+	touchText: { fontSize: fontSize, fontFamily: 'NanumSquareR' },
 	buttonText: {
 		fontSize: fontSize,
 		color: Colors.white,
 		right: 10,
+		fontFamily: 'NanumSquareR'
 	},
 	iconStyle: {
-		paddingRight: 20,
+		marginLeft: 10,
+		height: 25,
+		width: 25,
+		marginTop: 3,
+		marginRight: 5
 	},
 	touchableView: {
 		marginTop: 0,
@@ -507,20 +365,61 @@ const styles = StyleSheet.create({
 		paddingLeft: 30,
 
 		justifyContent: 'flex-start',
-		alignItems: 'center',
+		alignItems: 'center'
 		// flex: 1,
 	},
-	content: { padding: 5 },
+	content: { padding: 0 },
 	line: {
-		borderBottomWidth: 3,
+		borderBottomWidth: 3
 	},
 	switchText: {
 		fontSize: 20,
-		marginLeft: 17,
+		marginLeft: 17
 	},
 	circle: {
 		width: 9 * 2,
 		height: 9 * 2,
-		borderRadius: 9,
+		borderRadius: 9
 	},
+	rowView: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		width: '100%',
+		height: screen.height * 0.05,
+		// backgroundColor: backWhiteColor,
+		padding: 5
+	},
+	iconView: {
+		alignItems: 'flex-end',
+		flex: 1.5
+		// backgroundColor: 'transparent'
+	},
+	columnView: {
+		flexDirection: 'column',
+		alignContent: 'center'
+	},
+	rightIconStyle: {
+		marginRight: 10
+	},
+	backgroundView: {
+		borderRadius: 13
+		// backgroundColor: Colors.grey100
+	},
+	titleText: {
+		fontSize: 20,
+		alignSelf: 'flex-start',
+		fontFamily: 'NanumSquareBold',
+		letterSpacing: -1,
+		marginLeft: '5%',
+		marginTop: 20
+	},
+	blankView: {
+		height: 15
+	},
+	blankGreyView: {
+		height: 20,
+
+		marginTop: 20
+	}
 });
