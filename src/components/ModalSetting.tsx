4f4032843ee851da9048c32ffd302cd4eab27a37
ww8007 <wshmin@naver.com>
2022-03-01 23:28:14 +0900

    Feat: add ModalSetting 1. 화면 설정

---
+++ src/components/ModalSetting.tsx
@@ -0,0 +1,136 @@
+import React, { useCallback, useState } from 'react';
+import { Dimensions, StyleSheet, TouchableHighlight, View } from 'react-native';
+import { Colors } from 'react-native-paper';
+import { CloseButton, ModalView, Text } from '../theme';
+import Font5Icon from 'react-native-vector-icons/FontAwesome5';
+import { useToggleTheme } from '../contexts';
+import { useTheme } from '@react-navigation/native';
+
+const screen = Dimensions.get('screen');
+
+interface props {
+	modalVisible: boolean;
+	setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export function ModalSetting({ modalVisible, setModalVisible }: props) {
+	const [buttons] = useState([
+		{
+			radius: {
+				// borderBottomLeftRadius: 0,
+				// borderBottomRightRadius: 0
+			},
+			text: '화이트/다크 모드',
+			iconName: 'user-edit',
+			iconSize: 21,
+			onPressFunction: useToggleTheme()
+		}
+	]);
+	const onPressCloseBtn = useCallback(() => {
+		setModalVisible(false);
+	}, []);
+	const isDark = useTheme().dark;
+	return (
+		<ModalView
+			modalVisible={modalVisible}
+			setModalVisible={setModalVisible}
+			ModalViewRender={() => (
+				<>
+					<CloseButton closeBtn={onPressCloseBtn} />
+					<Text style={styles.titleText}>화면 설정</Text>
+					<View style={styles.blankView} />
+					<View style={styles.backgroundView}>
+						<View style={styles.columnView}>
+							{buttons.map((button) => (
+								<TouchableHighlight
+									activeOpacity={1}
+									underlayColor={isDark ? '#222831' : Colors.white}
+									onPress={button.onPressFunction}
+									key={button.text}
+									style={[styles.touchButtonStyle, button.radius]}
+								>
+									<View style={styles.rowView}>
+										<Font5Icon
+											name={button.iconName}
+											size={button.iconSize}
+											color={Colors.blue500}
+											style={styles.iconStyle}
+										/>
+										<Text style={[styles.touchText, { color: Colors.black }]}>
+											{' '}
+											{button.text}
+										</Text>
+										<View style={styles.iconView}>
+											<Font5Icon
+												name="angle-right"
+												size={19}
+												color={Colors.black}
+												style={styles.rightIconStyle}
+											/>
+										</View>
+									</View>
+								</TouchableHighlight>
+							))}
+						</View>
+					</View>
+				</>
+			)}
+		/>
+	);
+}
+
+const styles = StyleSheet.create({
+	titleText: {
+		fontSize: 20,
+		alignSelf: 'flex-start',
+		fontFamily: 'NanumSquareBold',
+		letterSpacing: -1,
+		marginLeft: '10%'
+	},
+	blankView: {
+		height: 15
+	},
+	backgroundView: {
+		borderRadius: 13,
+		backgroundColor: Colors.grey100
+	},
+	columnView: {
+		flexDirection: 'column',
+		alignContent: 'center'
+	},
+	touchButtonStyle: {
+		padding: 5,
+		borderRadius: 13,
+		justifyContent: 'center',
+		paddingLeft: 5,
+		paddingRight: 5
+	},
+	rowView: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		width: screen.width * 0.65,
+		height: screen.height * 0.05,
+		borderRadius: 13
+	},
+	iconStyle: {
+		marginLeft: 10
+	},
+	rightIconStyle: {
+		marginRight: 10
+	},
+	touchText: {
+		fontSize: 14,
+		textAlign: 'center',
+		fontFamily: 'NanumSquareR',
+		letterSpacing: -1,
+		position: 'absolute',
+		left: '16%',
+		justifyContent: 'center',
+		textAlignVertical: 'center'
+	},
+	iconView: {
+		alignItems: 'flex-end',
+		flex: 1
+	}
+});
