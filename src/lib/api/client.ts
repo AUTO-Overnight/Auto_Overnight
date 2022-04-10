05a5fbbd7c7426a1b87d2e932bb353b2e5b7ef76
장동현 <54137044+ww8007@users.noreply.github.com>
2022-04-11 01:06:12 +0900

    feat: Add checking env data

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_URL } from '@env';
 
 const client = axios.create();
-
+console.log(API_URL);
 client.defaults.baseURL = `${API_URL}`;
 
 export default client;
--- src/lib/api/weather.ts
+++ src/lib/api/weather.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { WEATHER_TOKEN, WEATHER_KEY } from '@env';
+
+console.log(WEATHER_KEY, WEATHER_TOKEN);
 export const getAirPollution = () => {
 	return axios.get(
 		`http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=${WEATHER_TOKEN}&returnType=json&numOfRows=1&pageNo=1&stationName=%EC%A0%95%EC%99%95%EB%8F%99&dataTerm=DAILY&ver=1.0`
