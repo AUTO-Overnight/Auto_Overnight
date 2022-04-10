865726937c59e6ca035c092f991149b42f795b34
장동현 <54137044+ww8007@users.noreply.github.com>
2022-04-11 01:05:57 +0900

    fix: GetBonus api parameter, data

--- src/lib/api/bonus.ts
+++ src/lib/api/bonus.ts
@@ -1,9 +1,11 @@
 import client from './client';
 import type { BonusAPI } from '../../interface';
-export const getBonus = ({ id, name, cookies }: BonusAPI) => {
+export const getBonus = ({ id, name, cookies, thisYear, tmGbn }: BonusAPI) => {
 	const data = JSON.stringify({
 		userNm: name,
 		cookies: cookies,
+		yy: thisYear,
+		tmGbn
 	});
 	return client.post('/findpointlist', data);
 };
