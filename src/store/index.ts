7efc3d031c950d823f170a164bee1e5d32042e73
장동현 <54137044+ww8007@users.noreply.github.com>
2022-04-10 19:14:20 +0900

    fix: Remove calendar reducer from persist blacList

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,7 +11,7 @@ import weather, { weatherSaga } from './weather';
 const persistConfig = {
 	key: 'root',
 	storage: AsyncStorage,
-	blacklist: ['calendar', 'bonus']
+	blacklist: ['bonus']
 };
 const rootReducer = combineReducers({
 	login,
