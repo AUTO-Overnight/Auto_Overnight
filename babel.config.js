b3a3900c690e8da0ffb42e46b8a4e42ad8c3c1c6
장동현 <54137044+ww8007@users.noreply.github.com>
2022-04-11 23:35:27 +0900

    chore: Remove babel/preset-env from presets

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,7 @@
 module.exports = function (api) {
 	api.cache(true);
 	return {
-		presets: [
-			'babel-preset-expo',
-			['@babel/preset-env', { targets: { node: 'current' } }],
-
-			'@babel/preset-typescript'
-		],
+		presets: ['babel-preset-expo', '@babel/preset-typescript'],
 		plugins: [
 			['@babel/plugin-proposal-private-property-in-object', { loose: true }]
 		]
