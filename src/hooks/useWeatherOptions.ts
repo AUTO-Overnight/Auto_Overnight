8fd1b5fe9e7b2d4b994ff15db92fbc33faf50bdd
ww8007 <wshmin1234@gmail.com>
2022-11-20 19:36:33 +0900

    style: prettier tabWidth 4로 변경

--- src/hooks/useWeatherOptions.ts
+++ src/hooks/useWeatherOptions.ts
@@ -1,20 +1,21 @@
 import { Colors } from 'react-native-paper';
 import { useSelector } from 'react-redux';
+import constColors from '../constants/colors';
 import { RootState } from '../store';
 
 const AirPollutionOptions = {
 	1: {
-		iconName: 'smiley',
+		iconName: 'smiley'
 	},
 	2: {
-		iconName: 'slightly-smile',
+		iconName: 'slightly-smile'
 	},
 	3: {
-		iconName: 'confused',
+		iconName: 'confused'
 	},
 	4: {
-		iconName: 'frowning',
-	},
+		iconName: 'frowning'
+	}
 };
 
 const weatherOptions = {
@@ -23,35 +24,35 @@ const weatherOptions = {
 		iconColor: Colors.grey500,
 		gradient: ['#4DA0B0', '#D39D38'],
 		title: '대체로 흐림',
-		subtitle: '흐린 날씨 입니다. 비가 올 수도 있어요.',
+		subtitle: '흐린 날씨 입니다. 비가 올 수도 있어요.'
 	},
 	Thunderstorm: {
 		iconName: 'weather-lightning',
 		iconColor: Colors.grey900,
 		gradient: ['#373B44', '#4286f4'],
 		title: '번개',
-		subtitle: '천둥 번개가 칩니다. 조심하세요!!!',
+		subtitle: '천둥 번개가 칩니다. 조심하세요!!!'
 	},
 	Drizzle: {
 		iconName: 'weather-hail',
 		iconColor: Colors.blue200,
 		gradient: ['#89F7FE', '#66A6FF'],
 		title: '이슬비 내림',
-		subtitle: '이슬비가 내립니다. 우산을 챙기세요!!!',
+		subtitle: '이슬비가 내립니다. 우산을 챙기세요!!!'
 	},
 	Rain: {
 		iconName: 'weather-rainy',
 		iconColor: Colors.blue400,
 		gradient: ['#00C6FB', '#005BEA'],
 		title: '비오는 날',
-		subtitle: '비가 내립니다. 우산을 챙기세요!!!',
+		subtitle: '비가 내립니다. 우산을 챙기세요!!!'
 	},
 	Snow: {
 		iconName: 'weather-snowy-heavy',
 		iconColor: Colors.white,
 		gradient: ['#7DEFC', ' #B9B6E5'],
 		title: '눈',
-		subtitle: '눈이 내려요. 미끄러지지 않게 조심하세요!!!',
+		subtitle: '눈이 내려요. 미끄러지지 않게 조심하세요!!!'
 	},
 	Clouds: {
 		iconName: 'weather-cloudy',
@@ -59,14 +60,14 @@ const weatherOptions = {
 		//gradient: ['#D7D2CC', '#304352'],
 		gradient: ['#304352', '#D7D2CC'],
 		title: '대체로 흐림',
-		subtitle: '선선한 날 이에요',
+		subtitle: '선선한 날 이에요'
 	},
 	atmosphere: {
 		iconName: 'weather-hail',
 		iconColor: Colors.white,
 		gradient: ['#4DA0B0', '#D39D38'],
 		title: '안개 낌',
-		subtitle: '우중충한 날이에요',
+		subtitle: '우중충한 날이에요'
 	},
 	Clear: {
 		iconName: 'weather-sunny',
@@ -74,24 +75,24 @@ const weatherOptions = {
 		iconColor: [Colors.yellow400, Colors.white],
 		//gradient: ['#FF7300', Colors.grey100],
 		gradient: [Colors.yellow400, Colors.orange900],
-		blackGradient: [Colors.black, Colors.grey500],
+		blackGradient: [constColors.mainDarkColor, Colors.grey500],
 		title: '아주 맑음',
-		subtitle: '여행 가기 좋은 날이에요',
+		subtitle: '여행 가기 좋은 날이에요'
 	},
 	Dust: {
 		iconName: 'weather-hail',
 		iconColor: Colors.white,
 		gradient: ['#4DA0B0', '#D39D38'],
 		title: '먼지 낌',
-		subtitle: '우중충한 날이에요',
+		subtitle: '우중충한 날이에요'
 	},
 	Mist: {
 		iconName: 'weather-hail',
 		iconColor: Colors.white,
 		gradient: ['#4DA0B0', '#D39D38'],
 		title: '안개 낌',
-		subtitle: '우중충한 날이에요',
-	},
+		subtitle: '우중충한 날이에요'
+	}
 };
 
 export function useWeatherOptions(): {
