70f6b874378323fee6347082612829a099289f27
ww8007 <wshmin1234@gmail.com>
2023-03-01 18:08:40 +0900

    fix: version 정보 받아오는 api 삭제

--- src/lib/api/login.ts
+++ src/lib/api/login.ts
@@ -1,17 +1,13 @@
 import client from './client';
 import axios from 'axios';
 import type { User } from '../../interface';
-import { VERSION_URL } from '@env';
-export const getLogin = ({ userId, userPw }: User) => {
-	userId = encodeURIComponent(userId);
-	userPw = encodeURIComponent(userPw);
-	const user = JSON.stringify({
-		id: userId,
-		password: userPw,
-	});
-	return client.post('/login', user);
-};
 
-export const getVersion = () => {
-	return axios.get(VERSION_URL);
+export const getLogin = ({ userId, userPw }: User) => {
+  userId = encodeURIComponent(userId);
+  userPw = encodeURIComponent(userPw);
+  const user = JSON.stringify({
+    id: userId,
+    password: userPw,
+  });
+  return client.post('/login', user);
 };
